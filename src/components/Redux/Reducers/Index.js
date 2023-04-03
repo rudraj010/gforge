5108c34eb4b2a8604a966a56bc945bc19039041b
rudraj010 <rajendrakumar25.rk11@gmail.com>
2023-04-03 11:30:06 +0530

    y

--- src/components/Redux/Reducers/Index.js
+++ src/components/Redux/Reducers/Index.js
@@ -2,7 +2,7 @@
 
 import { ADD_CART,REMOVE_ALL,REMOVE_SINGLE,EMPTY_ITEM } from "../Constant/Constant";
 const INIT_STATE = {
-    carts: localStorage.getItem("Localitems",) ? JSON.parse(localStorage.getItem("Localitems")) : []
+    carts: localStorage.getItem("Localitems") ? JSON.parse(localStorage.getItem("Localitems")) : []
 };
 
 console.log(INIT_STATE.carts, "sa")
@@ -23,16 +23,16 @@ export const cartreducer = (state = INIT_STATE, action) => {
                     
                 }
             } else {
-                const temp = { ...action.payload, qnty: 1 }
+                const x = { ...action.payload, qnty: 1 }
                 return {
                     ...state,
-                    carts: [...state.carts, temp]
+                    carts: [...state.carts, x]
                 }
 
             }
 
         case REMOVE_ALL:
-            const data = state.carts.filter((el) => el.id !== action.payload)
+            const data = state.carts.filter((item) => item.id !== action.payload)
             return {
                 ...state,
                 carts: data
@@ -40,7 +40,7 @@ export const cartreducer = (state = INIT_STATE, action) => {
             }
 
         case REMOVE_SINGLE:
-            const IteamIndex_dec = state.carts.findIndex((iteam) => iteam.id === action.payload.id);
+            const IteamIndex_dec = state.carts.findIndex((item) => item.id === action.payload.id);
 
             if (state.carts[IteamIndex_dec].qnty >= 1) {
                 const dltiteams = state.carts[IteamIndex_dec].qnty -= 1
